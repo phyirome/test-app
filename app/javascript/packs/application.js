06e6ab74c66948b54c7fef6cdb41add3457d4396
phyirome <hirohiroxmt@gmail.com>
2020-09-29 14:55:17 +0900

    ひとまず成功した実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require('../jquery.raty')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,7 +23,7 @@ require('jquery')
 //= require activestorage
 //= require jquery.raty.js
 
-console.log("hello")
+// console.log("hello")
 
 $(function () {
   $('#star').raty({
